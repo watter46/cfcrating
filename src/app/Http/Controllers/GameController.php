0c6dbdd97b72c80db1e2a8c97cfc427eb3f6032b
watter46 <w.aoki.14@gmail.com>
2024-09-12 01:42:10 +0900

    add: 指定のGameを取得して表示できるようにする

--- src/app/Http/Controllers/GameController.php
+++ src/app/Http/Controllers/GameController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Presenters\GamePresenter;
 use App\UseCases\User\FetchLatestGame;
+use App\UseCases\User\FindGame;
+
 
 class GameController extends Controller
 {
     public function __construct(
         private FetchLatestGame $fetchLatestGame,
+        private FindGame $findGame,
         private GamePresenter $gamePresenter
     ) {
         
@@ -22,6 +25,13 @@ class GameController extends Controller
     public function latest()
     {
         $game = $this->fetchLatestGame->execute();
+
+        return view('game', $this->gamePresenter->presentGame($game));
+    }
+
+    public function find(string $gameId)
+    {
+        $game = $this->findGame->execute($gameId);
 
         return view('game', $this->gamePresenter->presentGame($game));
     }
