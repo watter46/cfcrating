700da428a28fda8b9ab20b698d090c77e73f7c24
watter46 <w.aoki.14@gmail.com>
2024-09-27 04:48:21 +0900

    move: Admin用のViewディレクトリに移動する

--- src/resources/views/admin/games.blade.php
+++ src/resources/views/admin/games.blade.php
@@ -5,5 +5,7 @@
         </h2>
     </x-slot>
 
-    {{-- <livewire:user.games /> --}}
+    @foreach($games as $game)
+        <livewire:admin.game :$game :key="$game['id']" />
+    @endforeach
 </x-admin-layout>
