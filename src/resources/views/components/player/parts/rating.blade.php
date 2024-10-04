c0ee955d91c3a7d55081bd2d4f14bf1edf0b0e82
watter46 <w.aoki.14@gmail.com>
2024-10-04 21:47:03 +0900

    fix: Userのレスポンシブ対応する

--- src/resources/views/components/player/parts/rating.blade.php
+++ src/resources/views/components/player/parts/rating.blade.php
@@ -1,9 +1,9 @@
 @php
-    $textSize = 'text-xs md:text-xl rating-text';
+    $textSize = 'text-xs xs:text-sm md:text-xl rating-text';
 @endphp
 
 <div x-data
-    class="flex rating items-center justify-center px-1.5 md:px-3 rounded-xl min-w-7"
+    class="flex items-center justify-center px-2 rating md:px-3 rounded-xl min-w-7"
     :style="Boolean({{ $mom }})
         ? 'background-color: #0E87E0'
         : 'background-color: ' + ratingBgColor({{ $rating }})">
