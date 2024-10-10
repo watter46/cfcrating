e18282b8d156f0e08217752ea76c0b5fe690fc2a
watter46 <w.aoki.14@gmail.com>
2024-10-10 14:18:05 +0900

    add: Game一覧をUpdateできるようにする

--- src/resources/views/admin/games.blade.php
+++ src/resources/views/admin/games.blade.php
@@ -1,13 +1,18 @@
 <x-admin-layout>
     <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-            {{ __('Admin Games') }}
-        </h2>
+        <div class="flex justify-between w-full">
+            <h2 class="text-xl font-semibold leading-tight dark:text-gray-200">
+                {{ __('Admin Games') }}
+            </h2>
+    
+            <livewire:admin.update-games-button />
+        </div>
+        
     </x-slot>
 
     <section class="px-3">
         @foreach($games as $game)
-            <div class="border-b border-gray-500 cursor-pointer">
+            <div class="mb-1 cursor-pointer rounded-xl">
                 <a href="{{ route('admin.games.game', ['gameId' => $game['id']]) }}">
                     <x-game-summary.game-summary :$game />
                 </a>
--- src/resources/views/components/admin-key-form/update-button.blade.php
+++ src/resources/views/components/admin-key-form/update-button.blade.php
@@ -2,5 +2,5 @@
     x-data
     @click="$dispatch('open-admin-modal', { eventName:'{{ $eventName }}' })"
     {{ $attributes->class('px-3 py-1 rounded-lg bg-emerald-600 hover:bg-emerald-700') }}>
-    <p class="font-black text-gray-300">Update</p>
+    <p class="font-black text-gray-300">{{ $text ?? 'Update' }}</p>
 </button>
