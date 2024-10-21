5dbeb647f8d1db2b9f79abba5f5115e220fdfa96
watter46 <w.aoki.14@gmail.com>
2024-10-21 19:10:12 +0900

    add: UsersRatingを自動更新できるようにする

--- src/routes/console.php
+++ src/routes/console.php
@@ -1,8 +1,24 @@
-<?php
+<?php declare(strict_types=1);
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+use App\Jobs\UpdateGameJob;
+use App\Jobs\UpdateGamesJob;
+use App\Jobs\UpdateUsersRatingJob;
+
+/** 試合後にFixtureデータをAPIから取得してDBを更新 */
+Schedule::job(new UpdateGameJob)
+    ->when(fn() => UpdateGameJob::shouldScheduleJob())
+    ->everyTenMinutes();
+    
+/** FixturesデータをAPIから取得してDBを更新する */
+Schedule::job(new UpdateGamesJob)
+    ->days([1, 4]) // 月曜日と木曜日
+    ->at('23:00')
+    ->timezone('UTC');
+
+/** ユーザーの平均レーティングを更新する */
+Schedule::job(new UpdateUsersRatingJob)
+    ->when(fn() => UpdateUsersRatingJob::shouldScheduleJob())
+    ->dailyAt('04:00')
+    ->timezone('UTC');
