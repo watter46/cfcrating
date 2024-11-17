d5fa0d8d2fbb0b88aaa7fa78ac13b1c379efefd1
watter46 <w.aoki.14@gmail.com>
2024-11-17 15:19:50 +0900

    style: ダウンロードの時Goals Assistsのサイズを変更する

--- src/resources/views/components/player/parts/goals.blade.php
+++ src/resources/views/components/player/parts/goals.blade.php
@@ -1,14 +1,9 @@
-@php
-    $svgSize = '
-        w-[14px] h-[14px]
-        sm:w-[20px] sm:h-[20px]
-    ';
-@endphp
+@props(['svgSize' => 'size-[14px] sm:size-[20px]'])
 
 @if($goals)
     <div class="flex justify-center space-x-[-7px] sm:space-x-[-10px]">
         @foreach(range(1, $goals) as $num)
-            <div class="z-[{{ $loop->iteration }}] bg-gray-200 rounded-full p-0.5 goals {{ $svgSize }}">
+            <div class="z-[{{ $loop->iteration }}] bg-white rounded-full p-0.5 goals {{ $svgSize }}">
                 <x-svg.goal />
             </div>
         @endforeach
