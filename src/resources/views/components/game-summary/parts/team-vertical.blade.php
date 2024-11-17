47f4e0746ee9243cbbc02bd1119b8a3cb44c96de
watter46 <w.aoki.14@gmail.com>
2024-11-18 03:14:47 +0900

    style: フォーメーションの崩れを修正する

--- src/resources/views/components/game-summary/parts/team-vertical.blade.php
+++ src/resources/views/components/game-summary/parts/team-vertical.blade.php
@@ -1,7 +1,14 @@
-@props(['textSize' => 'text-lg', 'imgSize' => 'size-8'])
+@props(['textSize' => 'text-lg'])
+
+@php
+    $imgSize = 'h-16';
+    $bgSize = 'size-16';
+@endphp
 
 <div {{ $attributes->class("flex flex-col justify-start items-center space-y-2") }}>
-    <img src="{{ asset($team['path']) }}" class="{{ $imgSize }}">
+    <div class="grid content-center text-center justify-items-center">
+        <img src="{{ asset($team['path']) }}" class="{{ $imgSize }}">
+    </div>
 
     <p class="font-black text-gray-300 text-start text-wrap {{ $textSize }}">
         {{ $team['name'] }}
