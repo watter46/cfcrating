35fbc4339507161f3c21b5a666c3f40e1c3bd7ea
watter46 <w.aoki.14@gmail.com>
2024-11-19 04:49:25 +0900

    style: Lineupsを適応する

--- src/resources/views/components/result/image-previewer.blade.php
+++ src/resources/views/components/result/image-previewer.blade.php
@@ -3,7 +3,7 @@
         <x-game-summary.team-score-card :$game class="w-full" />
     </div>
     
-    <x-lineups.lineups :$game :maxWidth="600" fieldName="display">
+    <x-lineups.lineups :$game :maxWidth="600" playerComponent="player.display">
         <img id="result" src="{{ asset('storage/background/field.svg') }}" />
     </x-lineups.lineups>
 </div>
