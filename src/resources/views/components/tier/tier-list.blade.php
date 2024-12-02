561f5b907bc850d59386333b83fd4a7d7b284738
watter46 <w.aoki.14@gmail.com>
2024-12-03 00:58:14 +0900

    add: Downloadできるようにする

--- src/resources/views/components/tier/tier-list.blade.php
+++ src/resources/views/components/tier/tier-list.blade.php
@@ -17,16 +17,24 @@
     </section>
 
     <!-- Option -->
-    <div class="flex justify-end w-full mt-3 space-x-3">
-        <button class="relative size-10" @click="downloadTierImage()">
-            <x-svg.download class="size-full stroke-green-400 hover:stroke-green-300" />
-        </button>
+    <div class="flex justify-end w-full mt-3 space-x-7">
+        <div class="flex flex-col justify-center">
+            <button class="self-center size-9" @click="downloadTierImage()">
+                <x-svg.download class="self-center size-full stroke-gray-400 hover:stroke-gray-300" />
+            </button>
+
+            <p class="w-full text-xs font-black text-center text-gray-400">Download</p>
+        </div>
         
-        <button class="relative size-10" @click="add">
-            <span class="absolute inline-flex items-center justify-center px-1.5 text-sm font-black text-white bg-red-700 rounded-full -left-2 -top-2" x-text="remainingCount">
-            </span>
-            
-            <x-svg.plus class="size-full fill-gray-400 hover:fill-gray-300" />
-        </button>
+        <div class="flex flex-col justify-center">
+            <button class="relative self-center size-10" @click="add">
+                <span class="absolute inline-flex items-center justify-center px-1.5 text-sm font-black text-white bg-red-700 rounded-full -left-2 -top-2" x-text="remainingCount">
+                </span>
+                
+                <x-svg.plus class="self-center size-full fill-gray-400 hover:fill-gray-300" />
+            </button>
+
+            <p class="w-full text-xs font-black text-center text-gray-400">Add Tier</p>
+        </div>
     </div>
 </div>
--- src/resources/views/user/tier.blade.php
+++ src/resources/views/user/tier.blade.php
@@ -4,32 +4,113 @@
             
             <x-tier.tier-list />
             
-            <!-- Select Box -->
-            <div class="w-full mt-10 space-y-2">
+            <!-- Option -->
+            <section class="mt-5">
+                <div class="p-3 rounded-xl">
+                    <p class="text-xl text-gray-400">Player</p>
+
+                    <div class="flex items-center mt-3 space-x-5">
+                        <!-- Toggle Name -->
+                        <div class="flex flex-col justify-center space-y-2 w-fit">
+                            <x-ui.button.toggle-button
+                                x-data="{ isOn: true }"
+                                x-init="$watch('isOn', isOn => {
+                                    document
+                                        .querySelectorAll('.tier_player-name')
+                                        .forEach(player => player.classList.toggle('hidden'));
+                                })"
+                                class="self-center" />
+
+                            <p class="w-full text-sm font-black text-center text-gray-400">Name</p>
+                        </div>
+
+                        <!-- Toggle Number -->
+                        <div class="flex flex-col justify-center space-y-2 w-fit">
+                            <x-ui.button.toggle-button
+                                x-data="{ isOn: true }"
+                                x-init="$watch('isOn', isOn => {
+                                    document
+                                        .querySelectorAll('.tier_player-number')
+                                        .forEach(player => player.classList.toggle('hidden'));
+                                })"
+                                class="self-center" />
+
+                            <p class="w-full text-sm font-black text-center text-gray-400">Number</p>
+                        </div>
+
+                        <!-- Toggle Position -->
+                        <div class="flex flex-col justify-center space-y-2 w-fit">
+                            <x-ui.button.toggle-button
+                                x-data="{ isOn: true }"
+                                x-init="$watch('isOn', isOn => {
+                                    document
+                                        .querySelectorAll('.tier_player-position')
+                                        .forEach(player => player.classList.toggle('hidden'));
+                                })"
+                                class="self-center" />
+
+                            <p class="w-full text-sm font-black text-center text-gray-400">Position</p>
+                        </div>
+
+                        <!-- Toggle Frame -->
+                        <div class="flex flex-col justify-center space-y-2 w-fit">
+                            <x-ui.button.toggle-button
+                                x-data="{ isOn: true }"
+                                x-init="$watch('isOn', isOn => $dispatch('change-frame', isOn))"
+                                class="self-center" />
+
+                            <p class="w-full text-sm font-black text-center text-gray-400">Frame</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            
+            <!-- Select Player -->
+            <div class="w-full mt-3 space-y-2">
                 @foreach($positionGroups as $position => $positionGroup)
                     <div class="w-full rounded-xl">
-                        <x-player.parts.position :$position textSize="text-base" />
-
                         <div class="grid w-full grid-cols-12 min-h-28" x-init="initDraggableItem($el)">
                             @foreach($positionGroup as $player)
                                 <div class="flex flex-col justify-center w-full p-2 rounded-2xl">
                                     <div class="flex flex-col items-center justify-center w-full space-y-3">
                                         <x-player.player :$player size="size-16">
                                             <x-slot:frame>
-                                                <x-player.frames.normal />
-                                            </x-slot:frame>
+                                                <div x-data="{ isNormal: true }"
+                                                    x-init="window.addEventListener('change-frame', (e) => {
+                                                        isNormal = event.detail;
+                                                      });">
+                                                    <template x-if="isNormal">
+                                                        <x-player.frames.normal />
+                                                    </template>
 
+                                                    <template x-if="!isNormal">
+                                                        <x-player.frames.rounded />
+                                                    </template>
+                                                </div>
+                                            </x-slot:frame>
+                                            
+                                            <x-slot:top-left>
+                                                <x-player.parts.position
+                                                    :position="$player['position']"
+                                                    textSize="text-sm"
+                                                    class="tier_player-position" />
+                                            </x-slot:top-left>
+                                            
                                             <x-slot:bottom-right>
-                                                <p class="text-white bg-gray-600 rounded-full">
-                                                    {{ $player['number'] }}
-                                                </p>
+                                                @if($player['number'])
+                                                    <p class="text-center text-white bg-gray-600 rounded-full size-6 tier_player-number">
+                                                        {{ $player['number'] }}
+                                                    </p>
+                                                @endif
                                             </x-slot:bottom-right>
                                         </x-player.player>
                                         
-                                        <x-player.parts.name
-                                            :name="$player['name']"
-                                            :highlightName="false"
-                                            textSize="text-xs" />
+                                        <div class="tier_player-name">
+                                            <x-player.parts.name
+                                                :name="$player['name']"
+                                                :highlightName="false"
+                                                textSize="text-xs" />
+                                        </div>
                                     </div>
                                 </div>
                             @endforeach
