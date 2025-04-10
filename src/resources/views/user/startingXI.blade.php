129ca814c3cf03550348c07dba1869f755c55f74
watter46 <w.aoki.14@gmail.com>
2025-04-11 01:17:54 +0900

    style: モバイル環境でpaddingを下に入れるように修正する

--- src/resources/views/user/startingXI.blade.php
+++ src/resources/views/user/startingXI.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="flex flex-col justify-center w-full py-3">
+    <div class="flex flex-col justify-center w-full py-5">
         <!-- Lineups -->
         <x-startingXi.lineups :$players>
             <img lat="field" src="{{ asset('storage/background/field.svg') }}" />
