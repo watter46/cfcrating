5efac3a0cd94af03dcb9df805a6dd4ca19172805
watter46 <w.aoki.14@gmail.com>
2025-04-22 15:50:31 +0900

    fix: Resultの画像のレイアウトを修正する

--- src/resources/views/components/result/save-button.blade.php
+++ src/resources/views/components/result/save-button.blade.php
@@ -19,9 +19,9 @@
             substitutes.classList.toggle('hidden');
         })">
 
-        <x-result.image-previewer :$game class="lg:w-3/4" />
+        <x-result.image-previewer2 :$game class="lg:w-3/4" />
 
-        <div class="flex justify-center mt-5 lg:w-1/4 lg:mt-0 h-fit">
+        {{-- <div class="flex justify-center mt-5 lg:w-1/4 lg:mt-0 h-fit">
             <div class="flex flex-col justify-center w-full">
                 <!-- Options -->
                 <div class="flex w-full h-full items-center space-y-0.5 mb-10">
@@ -45,11 +45,44 @@
                     </x-result.copy-button>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 
-    {{-- <x-result.image-downloader :$game /> --}}
-    <x-result.image-downloader2 :$game class="w-full" />
+    <x-result.image-downloader :$game />
 </x-ui.modal.modal>
 
 @vite(['resources/js/downloadImage.js', 'resources/js/copyRatings.js'])
+
+
+
+{{-- <button x-ref="download_btn"
+    x-data="{
+        isProcessing: false,
+        isCompleted: false,
+    }"
+    :disabled="isProcessing"
+    :class="isProcessing && 'opacity-30'"
+    @click="
+        isProcessing = true;
+        $nextTick(() => {
+            downloadImage(includeSubs)
+                .then(() => {
+                    isProcessing = false;
+                    isCompleted = true;
+                    setTimeout(() => isCompleted = false, 2000);
+                })
+                .catch(() => {
+                    isProcessing = false;
+                });
+        });
+    "
+    {{ $attributes->class('relative z-0 h-10 overflow-hidden bg-black rounded-md btn btn-flat w-full') }}
+    style="--before-color: #16a34a"
+>
+    <span class="absolute inset-0 flex items-center justify-center w-full text-lg text-white gap-x-3">
+        {{ $slot }}
+        <p x-text="isProcessing ? 'downloading...' : (isCompleted ? 'Downloaded!!' : 'Download')"></p>
+    </span>
+</button>
+
+@vite(['resources/css/flow-button.css']) --}}
