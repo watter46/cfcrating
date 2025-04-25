a0dc9784ee10d37761002607ca927e8b4aeb4f80
watter46 <w.aoki.14@gmail.com>
2025-04-25 19:06:08 +0900

    fix: faviconを修正する

--- src/resources/views/layouts/user/app.blade.php
+++ src/resources/views/layouts/user/app.blade.php
@@ -5,14 +5,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>CFCRating - Chelsea FC Player Ratings, Man of the Match Votes & Match Recaps</title>
+        <meta name="description" content="Rate Chelsea FC players after every match, vote for the Man of the Match, and explore community-driven ratings and match summaries. Join fellow Blues fans and make your voice count!">
+        <link rel="canonical" href="https://cfcrating.com">
 
         <!-- favicon -->
+        <link rel="icon" href="{{ asset('/favicon/favicon.ico') }}" sizes="32x32" type="image/x-icon">
         <link rel="icon" href="{{ asset('/favicon/favicon.svg') }}" type="image/svg+xml">
-        <link rel="icon" href="{{ asset('/favicon/favicon.ico') }}" sizes="32x32" type="image/png">
-        <link rel="apple-touch-icon" href="{{ asset('/favicon/apple-touch-icon.png') }}" sizes="180x180">
         <link rel="icon" href="{{ asset('/favicon/android-chrome-192x192.png') }}" sizes="192x192" type="image/png">
-
+        <link rel="apple-touch-icon" href="{{ asset('/favicon/apple-touch-icon.png') }}" sizes="180x180">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -20,6 +21,24 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/css/background.css', 'resources/js/app.js'])
+
+        <script type="application/ld+json">
+            {!! json_encode([
+              "@context" => "https://schema.org",
+              "@type" => "WebSite",
+              "name" => "CFCRating",
+              "url" => "https://cfcrating.com",
+              "description" => 'Rate Chelsea FC players after each match, vote for Man of the Match.',
+              "publisher" => [
+                  "@type" => "Organization",
+                  "name" => "CFCRating",
+                  "logo" => [
+                      "@type" => "ImageObject",
+                      "url" => asset('/favicon/android-chrome-192x192.png')
+                  ]
+              ]
+            ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT) !!}
+        </script>
 
         @livewireStyles
     </head>
